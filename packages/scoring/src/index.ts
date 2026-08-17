38c723c648394963e7c4fbfaa26e5acf1f3cf583
MoraRosa <65734173+MoraRosa@users.noreply.github.com>
2026-08-17 02:59:44 +0000

    second commit

--- packages/scoring/src/index.ts
+++
@@ -1,2 +0,0 @@
-export * from "./computeQualification.js";
-export * from "./weights.js";
--- packages/shared-types/src/index.ts
+++
@@ -1,6 +0,0 @@
-export * from "./prospect.js";
-export * from "./research.js";
-export * from "./qualification.js";
-export * from "./activity.js";
-export * from "./opportunity.js";
-export * from "./catalog.js";
